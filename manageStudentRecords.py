b22d22766b10f0b946b63136592b430f86804e53
Gerry Huynh <gerry.huynh@shopify.com>
2024-03-15 13:30:40 -0400

    Finish getAllStudents()

--- manageStudentRecords.py
+++ manageStudentRecords.py
@@ -1,17 +1,49 @@
 import psycopg
 
-try:
-    conn = psycopg.connect(
-        "dbname='students' user='postgres' host='localhost' password='student1'"
-    )
-except psycopg.OperationalError as e:
-    print(f"Error: {e}")
-    exit(1)
+# PostgresSQL DB connection settings
+DB_NAME = "students"
+USER = "postgres"
+HOST = "localhost"
+PASSWORD = "student1"
 
-with conn.cursor() as cursor:
-    cursor.execute("SELECT * FROM students")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+"""
+getAllStudents(): Retrieves and displays all records from the students table.
+addStudent(first_name, last_name, email, enrollment_date): Inserts a new student record into the students table.
+updateStudentEmail(student_id, new_email): Updates the email address for a student with the specified student_id.
+deleteStudent(student_id): Deletes the record of the student with the specified student_id.
+"""
 
-conn.close()
+
+# Establishes connection to the students database
+# Returns a connection object
+def connect():
+    # connect to the students database
+    try:
+        conn = psycopg.connect(
+            f"dbname={DB_NAME} user={USER} host={HOST} password={PASSWORD}"
+        )
+    # if the connection fails, print the error and exit
+    except psycopg.OperationalError as e:
+        print(f"Error: {e}")
+        exit(1)
+    # return the connection object
+    return conn
+
+
+# Retrieves and displays all records from the students table
+# Returns nothing
+def getAllStudents():
+    conn = connect()
+    with conn.cursor() as cursor:
+        # query the students table
+        cursor.execute("SELECT * FROM students")
+        # fetch all the rows
+        rows = cursor.fetchall()
+        # print all the rows
+        for row in rows:
+            print(row)
+    conn.close()
+
+
+if __name__ == "__main__":
+    getAllStudents()
